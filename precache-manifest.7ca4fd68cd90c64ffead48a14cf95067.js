5df2437e87c196570a9631d1158c0c17e5685b86
Atizaz Ahsan <atizazahsan05@gmail.com>
2020-01-21 14:44:32 +0500

    Updates

---
+++ precache-manifest.7ca4fd68cd90c64ffead48a14cf95067.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c92dc8403d72eb8476cdb84797c23a8d",
+    "url": "/monster-rolex/index.html"
+  },
+  {
+    "revision": "062742550d6183d0fd99",
+    "url": "/monster-rolex/static/css/main.fcf32450.chunk.css"
+  },
+  {
+    "revision": "cb4891395d76421620b0",
+    "url": "/monster-rolex/static/js/2.202aa72a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolex/static/js/2.202aa72a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "062742550d6183d0fd99",
+    "url": "/monster-rolex/static/js/main.a2ddcf35.chunk.js"
+  },
+  {
+    "revision": "31aeed36a6ce02e4b192",
+    "url": "/monster-rolex/static/js/runtime-main.c1f6fdc9.js"
+  }
+]);
